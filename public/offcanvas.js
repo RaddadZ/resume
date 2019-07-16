f82952f10aa98e86c8e652fce05cbf7e3e9736ea
RaddadZ <ananraddad@gmail.com>
2019-07-16 09:48:00 +0300

    first base ready

--- public/offcanvas.js
+++ public/offcanvas.js
@@ -4,4 +4,16 @@ $(function () {
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
+  $(document).on('click','.feather-plus', function(event){
+    $(this).removeClass('feather-plus');
+    $(this).addClass('feather-minus');
+    $(this).attr('data-feather','minus');
+    feather.replace()
+  });
+  $(document).on('click','.feather-minus', function(event){
+    $(this).removeClass('feather-minus');
+    $(this).addClass('feather-plus');
+    $(this).attr('data-feather','plus');
+    feather.replace()
+  });
 })
